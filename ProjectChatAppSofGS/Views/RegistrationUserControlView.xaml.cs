2885ec2e549493f2bf1b47c32b6cb231cae580b1
Inchensenk <aab8117@gmail.com>
2023-03-03 17:17:10 +0300

    Редизайн окна Регистрации

--- ProjectChatAppSofGS/Views/RegistrationUserControlView.xaml.cs
+++ ProjectChatAppSofGS/Views/RegistrationUserControlView.xaml.cs
@@ -24,5 +24,10 @@ namespace Client.Views
         {
             InitializeComponent();
         }
+
+        private void OnBorderMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
